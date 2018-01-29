1b1ba5b385f385a365b63c4af487dde13e96eda8
Justin Fletcher <justinr.fletcher@gmail.com>
2018-01-28 20:13:20 -1000

    Sync commit.

--- queue_exhaustion_study_plot.py
+++ queue_exhaustion_study_plot.py
@@ -24,13 +24,17 @@ def bar_line_plot(ax1,
                   xmax,
                   ymax1,
                   ymax2,
+                  xlabel,
+                  ylabel_1,
+                  ylabel_2,
                   show_xlabel,
                   show_ylabel_1,
                   show_ylabel_2,
                   annotate_col,
                   col_annotation,
                   annotate_row,
-                  row_annotation):
+                  row_annotation,
+                  show_legends=True):
     """
     Parameters
     ----------
@@ -67,15 +71,15 @@ def bar_line_plot(ax1,
     ax2.set_xlim(xmin, xmax)
     ax2.set_ylim(ymin1, ymax1)
 
-    ax1.fill_between(time, 0, data2, color=c2, alpha=0.3)
+    ax1.fill_between(time, 0, data2, color=c2, label=ylabel_1, alpha=0.3)
 
     if show_xlabel:
-        ax1.set_xlabel('Training Step')
+        ax1.set_xlabel(xlabel)
     else:
         ax1.xaxis.set_ticklabels([])
 
     if show_ylabel_1:
-        ax1.set_ylabel('Queue Size (images)')
+        ax1.set_ylabel(ylabel_1)
     else:
         ax1.yaxis.set_ticklabels([])
 
@@ -86,6 +90,7 @@ def bar_line_plot(ax1,
                      data1,
                      color=c1,
                      alpha=0.5,
+                     label='Mean ' + ylabel_2,
                      zorder=0)
 
     errorfill(time,
@@ -93,10 +98,11 @@ def bar_line_plot(ax1,
               data1bar,
               color=line.get_color(),
               alpha_fill=0.3,
+              label=r'$\pm\sigma$ ' + ylabel_2,
               ax=ax2)
 
     if show_ylabel_2:
-        ax2.set_ylabel('Mean Single-Step \n Running Time (sec)')
+        ax2.set_ylabel(ylabel_2)
     else:
         ax2.yaxis.set_ticklabels([])
 
@@ -116,10 +122,14 @@ def bar_line_plot(ax1,
                      xycoords='axes fraction', textcoords='offset points',
                      size='large', ha='center', va='baseline')
 
+    if show_legends:
+        ax1.legend(loc=1)
+        ax2.legend(loc=2)
+
     return ax1, ax2
 
 
-def errorfill(x, y, yerr, color=None, alpha_fill=0.3, ax=None):
+def errorfill(x, y, yerr, color=None, alpha_fill=0.3, label=None, ax=None):
 
     ax = ax if ax is not None else plt.gca()
 
@@ -133,7 +143,12 @@ def errorfill(x, y, yerr, color=None, alpha_fill=0.3, ax=None):
         ymin, ymax = yerr
 
     # ax.plot(x, y)
-    ax.fill_between(x, ymax, ymin, alpha=alpha_fill, color=color,
+    ax.fill_between(x,
+                    ymax,
+                    ymin,
+                    alpha=alpha_fill,
+                    color=color,
+                    label=label,
                     zorder=0)
 
 
@@ -147,7 +162,7 @@ def single_plot_example():
 
     matplotlib.rcParams['text.usetex'] = True
     matplotlib.rcParams['text.latex.unicode'] = True
-    matplotlib.rcParams.update({'font.size': 12})
+    matplotlib.rcParams.update({'font.size': 14})
 
     plt.style.use('seaborn-whitegrid')
 
@@ -222,6 +237,9 @@ def single_plot_example():
                           xmax=1000,
                           ymax1=0.1,
                           ymax2=100000,
+                          xlabel='Training Step',
+                          ylabel_1='Queue Size (images)',
+                          ylabel_2='Single-Step Running Time (sec)',
                           show_xlabel=show_xlabel,
                           show_ylabel_1=show_ylabel_1,
                           show_ylabel_2=show_ylabel_2,
@@ -230,17 +248,17 @@ def single_plot_example():
                           annotate_row=annotate_row,
                           row_annotation=row_annotation)
 
-            plt.legend()
+            # plt.legend()
 
     plt.grid(True,
              zorder=0)
 
-    plt.legend(bbox_to_anchor=(0.5, 0.0),
-               loc="lower left",
-               mode="expand",
-               bbox_transform=fig.transFigure,
-               borderaxespad=0,
-               ncol=3)
+    # plt.legend(bbox_to_anchor=(0.5, 0.0),
+    #            loc="lower left",
+    #            mode="expand",
+    #            bbox_transform=fig.transFigure,
+    #            borderaxespad=0,
+    #            ncol=3)
 
     # plt.tight_layout(rect=(0.001, 0.001, 1.0, 1.0))
 
@@ -254,8 +272,6 @@ def single_plot_example():
     #             pad_inches=0.05)
     plt.show()
 
-
-
 # -------------------------------------------------
 # -------------------------------------------------
 # ----------Thread Count Column--------------------
@@ -266,7 +282,7 @@ def thread_count_col():
 
     matplotlib.rcParams['text.usetex'] = True
     matplotlib.rcParams['text.latex.unicode'] = True
-    matplotlib.rcParams.update({'font.size': 6})
+    matplotlib.rcParams.update({'font.size': 10})
 
     plt.style.use('seaborn-whitegrid')
 
@@ -325,33 +341,42 @@ def thread_count_col():
             step = run_df['step_num']
             step = run_df.groupby(['step_num'])['step_num'].mean().tolist()
 
-            bar_line_plot(ax1=ax,
-                          time=step,
-                          data1=mrt_mean,
-                          data1bar=mrt_std,
-                          data2=queue_size_mean,
-                          c1='r',
-                          c2='b',
-                          xmin=0,
-                          ymin1=0,
-                          ymin2=0,
-                          xmax=2500,
-                          ymax1=0.1,
-                          ymax2=100000,
-                          show_xlabel=show_xlabel,
-                          show_ylabel_1=show_ylabel_1,
-                          show_ylabel_2=show_ylabel_2,
-                          annotate_col=annotate_col,
-                          col_annotation=col_annotation,
-                          annotate_row=annotate_row,
-                          row_annotation=row_annotation)
-
-            plt.legend()
+            ax1, ax2 = bar_line_plot(ax1=ax,
+                                     time=step,
+                                     data1=mrt_mean,
+                                     data1bar=mrt_std,
+                                     data2=queue_size_mean,
+                                     c1='r',
+                                     c2='b',
+                                     xmin=0,
+                                     ymin1=0,
+                                     ymin2=0,
+                                     xmax=2500,
+                                     ymax1=0.1,
+                                     ymax2=100000,
+                                     xlabel='Training Step',
+                                     ylabel_1='Queue Size (images)',
+                                     ylabel_2='Single-Step \n Running Time (sec)',
+                                     show_legends=False,
+                                     show_xlabel=show_xlabel,
+                                     show_ylabel_1=show_ylabel_1,
+                                     show_ylabel_2=show_ylabel_2,
+                                     annotate_col=annotate_col,
+                                     col_annotation=col_annotation,
+                                     annotate_row=annotate_row,
+                                     row_annotation=row_annotation)
 
     plt.grid(True,
              zorder=0)
 
-    plt.legend(bbox_to_anchor=(0.5, 0.0),
+    ax1.legend(bbox_to_anchor=(0.2, 0.05),
+               loc="lower left",
+               mode="expand",
+               bbox_transform=fig.transFigure,
+               borderaxespad=0,
+               ncol=3)
+
+    ax2.legend(bbox_to_anchor=(0.6, 0.05),
                loc="lower left",
                mode="expand",
                bbox_transform=fig.transFigure,
@@ -360,7 +385,7 @@ def thread_count_col():
 
     # plt.tight_layout(rect=(0.001, 0.001, 1.0, 1.0))
 
-    fig.set_size_inches(6, 9)
+    fig.set_size_inches(9, 15)
 
     plt.suptitle(r"Running Time ($\mu\pm\sigma$) and Queue Size vs. Training Step")
     # fig.savefig('C:\\Users\\Justi\\Research\\61\\synaptic_annealing\\figures\\deep_sa_comparative_study.eps',
@@ -383,7 +408,7 @@ def batch_interval_vs_threads_plot():
 
     matplotlib.rcParams['text.usetex'] = True
     matplotlib.rcParams['text.latex.unicode'] = True
-    matplotlib.rcParams.update({'font.size': 6})
+    matplotlib.rcParams.update({'font.size': 10})
 
     plt.style.use('seaborn-whitegrid')
 
@@ -393,16 +418,11 @@ def batch_interval_vs_threads_plot():
     df = df.loc[(df.batch_interval != 128)]
     df = df.loc[(df.train_enqueue_threads != 128)]
 
-
     row_content = df.train_enqueue_threads
     row_levels = row_content.unique()
 
     col_content = df.batch_interval
     col_levels = col_content.unique()
-
-    intraplot_content = df.train_batch_size
-    intraplot_levels = intraplot_content.unique()
-
 
     fig = plt.figure()
 
@@ -445,42 +465,52 @@ def batch_interval_vs_threads_plot():
             step = run_df['step_num']
             step = run_df.groupby(['step_num'])['step_num'].mean().tolist()
 
-            bar_line_plot(ax1=ax,
-                          time=step,
-                          data1=mrt_mean,
-                          data1bar=mrt_std,
-                          data2=queue_size_mean,
-                          c1='r',
-                          c2='b',
-                          xmin=0,
-                          ymin1=0,
-                          ymin2=0,
-                          xmax=1000,
-                          ymax1=0.1,
-                          ymax2=100000,
-                          show_xlabel=show_xlabel,
-                          show_ylabel_1=show_ylabel_1,
-                          show_ylabel_2=show_ylabel_2,
-                          annotate_col=annotate_col,
-                          col_annotation=col_annotation,
-                          annotate_row=annotate_row,
-                          row_annotation=row_annotation)
+            ax1, ax2 = bar_line_plot(ax1=ax,
+                                     time=step,
+                                     data1=mrt_mean,
+                                     data1bar=mrt_std,
+                                     data2=queue_size_mean,
+                                     c1='r',
+                                     c2='b',
+                                     xmin=0,
+                                     ymin1=0,
+                                     ymin2=0,
+                                     xmax=1000,
+                                     ymax1=0.1,
+                                     ymax2=100000,
+                                     xlabel='Training Step',
+                                     ylabel_1='Queue Size (images)',
+                                     ylabel_2='Single-Step \n Running Time (sec)',
+                                     show_xlabel=show_xlabel,
+                                     show_ylabel_1=show_ylabel_1,
+                                     show_ylabel_2=show_ylabel_2,
+                                     annotate_col=annotate_col,
+                                     col_annotation=col_annotation,
+                                     annotate_row=annotate_row,
+                                     row_annotation=row_annotation,
+                                     show_legends=False)
 
-            plt.legend()
+            # plt.legend()
 
     plt.grid(True,
              zorder=0)
-
-    plt.legend(bbox_to_anchor=(0.5, 0.0),
+    ax1.legend(bbox_to_anchor=(0.37, 0.03),
                loc="lower left",
                mode="expand",
                bbox_transform=fig.transFigure,
                borderaxespad=0,
-               ncol=7)
+               ncol=3)
 
-    plt.tight_layout(pad=0.0, w_pad=0.0, h_pad=0.0)
+    ax2.legend(bbox_to_anchor=(0.55, 0.025),
+               loc="lower left",
+               mode="expand",
+               bbox_transform=fig.transFigure,
+               borderaxespad=0,
+               ncol=3)
 
-    fig.set_size_inches(16, 9)
+    # plt.tight_layout(pad=0.0, w_pad=0.0, h_pad=0.0)
+
+    fig.set_size_inches(20, 15)
 
     plt.suptitle("Queue Size and Single-Step Running Time for Combinations of Batch Interval and Enqueue Threads")
     # fig.savefig('C:\\Users\\Justi\\Research\\61\\synaptic_annealing\\figures\\deep_sa_comparative_study.eps',
@@ -501,7 +531,7 @@ def queue_rate_surface():
 
     matplotlib.rcParams['text.usetex'] = True
     matplotlib.rcParams['text.latex.unicode'] = True
-    matplotlib.rcParams.update({'font.size': 6})
+    matplotlib.rcParams.update({'font.size': 12})
 
     plt.style.use('seaborn-whitegrid')
 
@@ -522,10 +552,6 @@ def queue_rate_surface():
 
     col_content = df.batch_interval
     col_levels = col_content.unique()
-
-    intraplot_content = df.train_batch_size
-    intraplot_levels = intraplot_content.unique()
-
 
     enqueue_rates = []
     X = []
@@ -552,8 +578,14 @@ def queue_rate_surface():
                            Y,
                            enqueue_rates,
                            cmap=cm.coolwarm,
-                           linewidth=0.1,
+                           linewidth=0.0,
                            antialiased=True)
+
+    ax.set_xlabel('Thread Count')
+
+    ax.set_ylabel('Batch Interval')
+
+    ax.set_zlabel('Net Enquque Rate (images/sec)')
 
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
@@ -570,7 +602,7 @@ def running_time_surface():
 
     matplotlib.rcParams['text.usetex'] = True
     matplotlib.rcParams['text.latex.unicode'] = True
-    matplotlib.rcParams.update({'font.size': 6})
+    matplotlib.rcParams.update({'font.size': 12})
 
     plt.style.use('seaborn-whitegrid')
 
@@ -587,10 +619,6 @@ def running_time_surface():
 
     col_content = df.batch_interval
     col_levels = col_content.unique()
-
-    intraplot_content = df.train_batch_size
-    intraplot_levels = intraplot_content.unique()
-
 
     mean_running_times = []
     X = []
@@ -619,19 +647,24 @@ def running_time_surface():
                            linewidth=0.1,
                            antialiased=True)
 
+    ax.set_xlabel('Thread Count')
+
+    ax.set_ylabel('Batch Interval')
+
+    ax.set_zlabel('Mean Running Time')
+
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
 
+# -------------------------------------------------
+# -------------------------------------------------
+# -------------------------------------------------
+# -------------------------------------------------
+# -------------------------------------------------
+# -------------------------------------------------
+# -------------------------------------------------
+# -------------------------------------------------
 
-
-# -------------------------------------------------
-# -------------------------------------------------
-# -------------------------------------------------
-# -------------------------------------------------
-# -------------------------------------------------
-# -------------------------------------------------
-# -------------------------------------------------
-# -------------------------------------------------
 
 def generalization_stability_plot():
 
@@ -722,8 +755,8 @@ def generalization_stability_plot():
 # -------------------------------------------------
 
 single_plot_example()
-# thread_count_col()
-# batch_interval_vs_threads_plot()
-# queue_rate_surface()
-# running_time_surface()
-# generalization_stability_plot()
+thread_count_col()
+batch_interval_vs_threads_plot()
+queue_rate_surface()
+running_time_surface()
+generalization_stability_plot()
