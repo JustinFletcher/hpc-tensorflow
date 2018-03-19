42f7e9b581b1d904d14b0a259c9fc067a073da77
justinfletcher <justinr.fletcher@gmail.com>
2018-03-18 15:41:45 -1000

    Reduced args to account for tfs brittle argparse.

--- resnet_cifar_study/resnet_cifar_experiment.py
+++ resnet_cifar_study/resnet_cifar_experiment.py
@@ -14,18 +14,14 @@ def tensorflow_experiment():
 
     tf.gfile.MakeDirs(FLAGS.log_dir)
 
-    # Run experiment.
-    # FLAGS.train_script
-    print("Printing FLAGS here:")
-    # print(parser.parse_args())
-    # print(vars(FLAGS))
+    train_script='/gpfs/home/fletch/hpc-tensorflow/resnet_cifar_study/models/official/resnet/cifar10_main.py'
 
     flags_string = ""
     for key, value in vars(FLAGS).items():
         flags_string += " --%s=%s" % (key, value)
     print(flags_string)
 
-    print("I want to run: %s" % FLAGS.train_script)
+    print("I want to run: %s" % train_script)
     os.system("python %s %s" % (FLAGS.train_script, flags_string))
     print("I tried...")
 
@@ -90,86 +86,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # Establish default arguements.
-    parser.add_argument('--train_script', type=str,
-                        default='/gpfs/home/fletch/hpc-tensorflow/resnet_cifar_study/models/official/resnet/cifar10_main.py',
-                        help='Official train file.')
-
-    parser.add_argument('--train_dir', type=str,
+    parser.add_argument('--model_dir', type=str,
                         default='/gpfs/projects/ml/tfmodels/resnet_cifar_model/',
                         help='Model checkpoint and event directory.')
-
-    # These flags are often, but not always, overwritten by the launcher.
-    parser.add_argument('--log_dir', type=str,
-                        default='/gpfs/projects/ml/log/tmp/resnet_cifar_study/',
-                        help='Summaries log directory.')
-
-    parser.add_argument('--pause_time', type=float,
-                        default=0.0,
-                        help='Number of seconds to pause before execution.')
-
-    parser.add_argument('--log_filename', type=str,
-                        default='deep_sa_generalization_experiment.csv',
-                        help='Summaries log directory.')
-
-    parser.add_argument('--keep_prob', type=float,
-                        default=1.0,
-                        help='Keep probability for output layer dropout.')
-
-    parser.add_argument('--train_batch_size', type=int,
-                        default=128,
-                        help='Training set batch size.')
-
-    parser.add_argument('--batch_interval', type=int,
-                        default=1,
-                        help='Interval between training batch refresh.')
-
-    parser.add_argument('--max_steps', type=int, default=1000,
-                        help='Number of steps to run trainer.')
-
-    parser.add_argument('--test_interval', type=int, default=100,
-                        help='Number of steps between test set evaluations.')
-
-    parser.add_argument('--learning_rate', type=float, default=1e-4,
-                        help='Initial learning rate')
 
     # These flags specify the data used in the experiment.
     parser.add_argument('--data_dir', type=str,
                         default='/gpfs/projects/ml/data/cifar10_official',
                         help='Directory from which to pull data TFRecords.')
 
-    parser.add_argument('--train_file', type=str,
-                        default='train.tfrecords',
-                        help='Training dataset filename.')
 
-    parser.add_argument('--validation_file', type=str,
-                        default='validation.tfrecords',
-                        help='Validation dataset filename.')
-
-    parser.add_argument('--input_size', type=int,
-                        default=28 * 28,
-                        help='Dimensionality of the input space.')
-
-    parser.add_argument('--label_size', type=int,
-                        default=10,
-                        help='Dimensinoality of the output space.')
-
-    parser.add_argument('--val_batch_size', type=int,
-                        default=10000,
-                        help='Validation set batch size.')
-
-    # These flags control the input pipeline threading.
-    parser.add_argument('--val_enqueue_threads', type=int,
-                        default=32,
-                        help='Number of threads to enqueue val examples.')
-
-    parser.add_argument('--train_enqueue_threads', type=int,
-                        default=128,
-                        help='Number of threads to enqueue train examples.')
-
-    # These flags specify placekeeping variables.
-    parser.add_argument('--rep_num', type=int,
-                        default=0,
-                        help='Flag identifying the repitition number.')
 
     # Parse known arguements.
     FLAGS, unparsed = parser.parse_known_args()
