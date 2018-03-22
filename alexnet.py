f6ad2a53c739c22b8b071e26e189dd24e9f1cedc
justinfletcher <justinr.fletcher@gmail.com>
2018-03-21 22:21:15 -1000

    Debug

--- alexnet.py
+++ alexnet.py
@@ -253,6 +253,6 @@ class AlexNetTensorFlowModel(TensorFlowModel):
         # output, end_points = alexnet_v2(self.stimulus_placeholder)
 
         with slim.arg_scope(alexnet_v2_arg_scope()):
-            outputs, _ = alexnet_v2(inputs=images_re, global_pool=True)
+            outputs, _ = alexnet_v2(inputs=images_re)
         return(outputs)
         ####################
